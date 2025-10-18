9365525d81f2c9dfefba7c6f45a836232bbba67e
G. Pape <pape@smarden.org>
2025-10-18 11:51:14 +0200

    * tcpsvd.c, udpsvd.c: use parameter name "unused" in sighandler functions
    NetBSD complains if a parameter name is omitted.

--- src/tcpsvd.c
+++ src/tcpsvd.c
@@ -113,14 +113,14 @@ void connection_status() {
   out(bufnum); flush("\n");
 }
 
-void sig_term_handler(int) {
+void sig_term_handler(int unused) {
   if (phccmax) ipsvd_phcc_free();
   if (verbose) {
     out(INFO); flush("sigterm received, exit.\n");
   }
   _exit(0);
 }
-void sig_child_handler(int) {
+void sig_child_handler(int unused) {
   int wstat;
   int i;
 
